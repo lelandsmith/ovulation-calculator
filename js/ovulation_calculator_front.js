6307ef44140fabad5519d755284514d1dbd023b0
Zakir Sajib <zakirsajib@gmail.com>
2018-04-25 18:19:29 +0100

    no message

--- js/ovulation_calculator_front.js
+++ js/ovulation_calculator_front.js
@@ -43,19 +43,13 @@ $(function ($) {
 		$('.ui-datepicker-next, .ui-datepicker-prev').live('click', function(){
 			
 			setTimeout(function() {
-				$( "td.periodDay:eq( 0 )" ).css( "opacity", "1" );
-				$( "td.periodDay:eq( 1 )" ).css( "opacity", "0.8" );
-				$( "td.periodDay:eq( 2 )" ).css( "opacity", "0.6" );
-				$( "td.periodDay:eq( 3 )" ).css( "opacity", "0.4" );
-				$( "td.periodDay:eq( 4 )" ).css( "opacity", "0.2" );
-				$( "td.periodDay:eq( 5 )" ).css( "opacity", "0.1" );
+				$('.periodDay').each(function (i) {
+					$(this).addClass('periodDay-' + i);
+				});
 				
-				$( "td.fertileDay:eq( 0 )" ).css( "opacity", "1" );
-				$( "td.fertileDay:eq( 1 )" ).css( "opacity", "0.8" );
-				$( "td.fertileDay:eq( 2 )" ).css( "opacity", "0.6" );
-				$( "td.fertileDay:eq( 3 )" ).css( "opacity", "0.4" );
-				$( "td.fertileDay:eq( 4 )" ).css( "opacity", "0.2" );
-				$( "td.fertileDay:eq( 5 )" ).css( "opacity", "0.1" );
+				$('.fertileDay').each(function (x) {
+					$(this).addClass('fertileDay-' + x);
+				});
 			
 			}, 500);
 			
